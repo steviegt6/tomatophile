b12939d2cee3aff79a2c7e22a1677713ec92b79b
Steviegt6 <xxlennygamerxx@gmail.com>
2021-12-22 17:05:32 -0800

    Display the scroll bar at 100% completion if the page isn't scrollable.

--- components/progressBar.js
+++ components/progressBar.js
@@ -5,7 +5,11 @@ export function startTimer() {
     let winHeight = window.innerHeight;
     let scrollPercent = scrollTop / (docHeight - winHeight);
     let scrollPercentRounded = /*Math.round(*/scrollPercent * 100/*)*/;
-  
+
+    if (winHeight >= docHeight) {
+      scrollPercentRounded = 100;
+    }
+
     document.getElementById("progress-bar").style.width =
       scrollPercentRounded + "%";
   }, 1);
