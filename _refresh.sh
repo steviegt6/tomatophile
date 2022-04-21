3745b8bf0acb98645d89c1bd09ce4fb124e6a26f
Steviegt6 <xxlennygamerxx@gmail.com>
2022-04-20 23:21:47 -0700

    #

--- _refresh.sh
+++ _refresh.sh
@@ -1,3 +1,3 @@
-rm -rf ./modules/notes
-git clone https://github.com/uranometrical/notes ./modules/notes
-git clone https://github.com/rejuvena/tml-wiki ./modules/tml-wiki
+#rm -rf ./modules/notes
+#git clone https://github.com/uranometrical/notes ./modules/notes
+#git clone https://github.com/rejuvena/tml-wiki ./modules/tml-wiki
