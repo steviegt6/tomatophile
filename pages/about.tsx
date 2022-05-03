dae069a95e32cfe94246daab58857c440dd631ab
Steviegt6 <xxlennygamerxx@gmail.com>
2022-05-02 18:53:15 -0700

    Adjust background transparency, fix stray apostrophe issue.

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,7 +5,7 @@ export default function About() {
         <GenericLayout>
             <h1>About</h1>
             <p>This site is fully open-source under the MIT License <a href="https://github.com/Steviegt6/tomatophile">on GitHub</a>. Various assets may not be as open.</p>
-            <p>This websites serves the purpose of a portfolio and easy place to host various creations of mine. There isn%apos;t any single purpose.</p>
+            <p>This websites serves the purpose of a portfolio and easy place to host various creations of mine. There isn&apos;t any single purpose.</p>
             <br />
             <h2>Sources</h2>
             <p>The background image used on this site was created by <a href="https://koba10212110.wixsite.com/sakeharasu">sakaharasu</a>.</p>
