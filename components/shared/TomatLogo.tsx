8d2cef7e51e90152b38f12085cc309973606ef6a
Tomat <xxlennygamerxx@gmail.com>
2022-06-05 22:43:14 -0700

    PFP adjustements.

--- components/shared/TomatLogo.tsx
+++ components/shared/TomatLogo.tsx
@@ -1,21 +1,40 @@
 import Image from "next/image";
 import Link from "next/link";
-import TomatPfp from "../../public/images/pfp.png";
+import pfp from "../../public/images/pfp.png";
 import styles from "./tomatlogo.module.scss";
 
 export default function TomatLogo({ width, height }) {
   return (
-    <Link href="/">
-      <a className={styles.tomatLink + " center-flexible"}>
-        <Image
-          src={TomatPfp}
-          width={width}
-          height={height}
-          alt="My Profile Picture"
-          className={styles.profilePicture}
-        />
-        <p className={styles.greetingText}>Hi there, I'm Tomat.</p>
-      </a>
-    </Link>
+    <div className="center-flexible">
+      <Link href="/">
+        <a>
+          <TomatPfp width={width} height={height} />
+        </a>
+      </Link>
+      <Greeting />
+    </div>
+  );
+}
+
+function TomatPfp({ width, height }) {
+  return (
+    <Image
+      src={pfp}
+      width={width}
+      height={height}
+      alt="My Profile Picture"
+      className={styles.profilePicture}
+    />
+  );
+}
+
+function Greeting() {
+  return (
+    <p className={styles.greetingText}>
+      Hi there,{" "}
+      <Link href="/">
+        <a className={styles.tomatLink}>I'm Tomat.</a>
+      </Link>
+    </p>
   );
 }
