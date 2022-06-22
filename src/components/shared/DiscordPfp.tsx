6dfc2f35c8617d60edc9ecf80e84810049d2b578
Tomat <xxlennygamerxx@gmail.com>
2022-06-22 15:05:20 -0700

    blah blah blah fadein blah

--- src/components/shared/DiscordPfp.tsx
+++ src/components/shared/DiscordPfp.tsx
@@ -1,29 +1,67 @@
 import { useEffect, useState } from "react";
 import { getDiscordProfileUrl } from "../../../lib/discordProfile";
-import LoadingSpinner from "./LoadingSpinner";
 
 const discordId = "269903979582849024";
 
-export default function DiscordPfp({ width, height }) {
-  const [image, setImage] = useState(<LoadingSpinner width={width} height={height} />);
+export interface Props {
+  width: number;
+  height: number;
+  overrideUrl: string | undefined;
+}
 
-  useEffect(() => {
-    const doAsync = async () => {
-      setImage(
-        <div style={{ width: width, height: height }}>
-          <img
-            className="flex justify-center items-center rounded-full"
-            src={await getDiscordProfileUrl(discordId)}
-            width={width}
-            height={height}
-            alt={""}
-          ></img>
-        </div>
-      );
-    };
+interface ImageProps {
+  width: number;
+  height: number;
+  src: string;
+}
 
-    doAsync();
-  });
+export default function DiscordPfp({
+  width,
+  height,
+  overrideUrl = undefined,
+}: Props) {
+  const useOverride = overrideUrl !== undefined;
+  const [image, setImage] = useState(
+    useOverride ? (
+      <Image width={width} height={height} src={overrideUrl} />
+    ) : (
+      <p>Remind me to put something here.</p>
+    )
+  );
+
+  if (!useOverride) {
+    useEffect(() => {
+      const doAsync = async () => {
+        setImage(
+          <div style={{ width: width, height: height }}>
+            <img
+              className="flex justify-center items-center rounded-full"
+              src={await getDiscordProfileUrl(discordId)}
+              width={width}
+              height={height}
+              alt={""}
+            ></img>
+          </div>
+        );
+      };
+
+      doAsync();
+    });
+  }
 
   return image;
+}
+
+function Image({ width, height, src }: ImageProps) {
+  return (
+    <div style={{ width: width, height: height }}>
+      <img
+        className="flex justify-center items-center rounded-full"
+        src={src}
+        width={width}
+        height={height}
+        alt={""}
+      ></img>
+    </div>
+  );
 }
